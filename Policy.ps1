660c160f15c9de69006c0d261b7e2d848550423a
Edison Arley Plaza Marin <edison954@gmail.com>
2020-05-21 22:23:58 -0500

    seccion 4

---
+++ Policy.ps1
@@ -0,0 +1,25 @@
+﻿#Connect-AzAccount
+#$Credentials = Get-Credential
+#Get-AzVM -Status
+#dir
+
+# Create the Policy Definition
+#$definition = New-AzPolicyDefinition -Name "contain-resource-group-name" -DisplayName "Resource name contains resource group name" -description "Require resources to contain the resource group's name" -Policy 'https://raw.githubusercontent.com/Azure/azure-policy/master/samples/TextPatterns/contain-resource-group-name/azurepolicy.rules.json' -Mode Indexed
+
+# Show Definition
+#$definition
+
+# Create the Policy Assignment
+#$assignment = New-AzPolicyAssignment -Name <assignmentname> -Scope <scope> -PolicyDefinition $definition
+
+## Show Assignment
+#$assignment
+
+$definition = New-AzPolicyDefinition -Name "enforce-match-pattern" -DisplayName "Ensure resource names match the naming pattern" -description "Ensure resource names match the naming pattern" -Policy 'https://raw.githubusercontent.com/Azure/azure-policy/master/samples/TextPatterns/enforce-match-pattern/azurepolicy.rules.json' -Parameter 'https://raw.githubusercontent.com/Azure/azure-policy/master/samples/TextPatterns/enforce-match-pattern/azurepolicy.parameters.json' -Mode All
+$definition
+#$assignment = New-AzPolicyAssignment -Name <assignmentname> -Scope <scope> -PolicyDefinition $definition
+#$assignment 
+
+
+$definition = New-AzPolicyDefinition -Name "enforce-like-pattern" -DisplayName "Ensure resource names meet the like condition for a pattern." -description "Ensure resource names meet the like condition for a pattern." -Policy 'https://raw.githubusercontent.com/Azure/azure-policy/master/samples/TextPatterns/enforce-like-pattern/azurepolicy.rules.json' -Parameter 'https://raw.githubusercontent.com/Azure/azure-policy/master/samples/TextPatterns/enforce-like-pattern/azurepolicy.parameters.json' -Mode All
+$definition
---
+++ virtualmachine.ps1
@@ -0,0 +1,18 @@
+﻿$RG2 = New-AzureRmResourceGroup -Name "Az100RG2" -Location "Southcentral US"
+$Credentials = Get-Credential
+
+#CREAR MAQUINA VIRTUAL 1
+$ASet = New-AzureRmAvailabilitySet -ResourceGroupName $RG2.ResourceGroupName -Name "aset1" -Location $RG2.Location -Sku "Aligned" -PlatformFaultDomainCount 1 -platformUpdateDomainCount 1
+#$AvailabilitySet = Get-AzureRmAvailabilitySet -ResourceGroupName $RG2.ResourceGroupName -Name "aset1"
+$VMConfig1 = New-AzureRmVMConfig -VMName "VMAz101" -VMSize "Standard_A1" -AvailabilitySetId $ASet.Id
+$VMConfig1 = Set-AzureRmVMOperatingSystem -VM $VMConfig1 -Linux -ComputerName "VMAz101" -Credential ($Credentials)
+$VMConfig1 = Set-AzureRmVMSourceImage -VM $VMConfig1 -PublisherName "Canonical" -Offer "UbuntuServer" -Skus "14.04.2-LTS" -Version "latest"
+$VMConfig1 = Set-AzureRmVMOSDisk -VM $VMConfig1 -Name "DiskOS2" -Caching ReadWrite -CreateOption FromImage -Linux
+$subnet1 = New-AzureRmVirtualNetworkSubnetConfig -Name "Subnet3" -AddressPrefix "172.16.3.0/24"
+$subnet2 = New-AzureRmVirtualNetworkSubnetConfig -Name "Subnet4" -AddressPrefix "172.16.4.0/24"
+$vnet = New-AzureRmVirtualNetwork -Name "VNet2" -ResourceGroupName $RG2.ResourceGroupName -Location $RG2.Location -AddressPrefix "172.16.0.0/16" -Subnet ($subnet1,$subnet2)
+$pip = New-AzureRmPublicIpAddress -ResourceGroupName $RG2.ResourceGroupName -Name "PIP2" -Location $RG2.Location -AllocationMethod Static
+$nic = New-AzureRmNetworkInterface -Name "NIC2" -ResourceGroupName $RG2.ResourceGroupName -Location $RG2.Location -SubnetId $vnet.Subnets[0].Id -PublicIpAddressId $pip.Id
+$VMConfig1 = Add-AzureRmVMNetworkInterface -VM $VMConfig1 -Id $nic.Id -Primary
+
+New-AzureRmVM -ResourceGroupName $RG2.ResourceGroupName -Location $RG2.Location -VM $VMConfig1
